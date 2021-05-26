98bbd1f9478d4f3c96afefc3cf2ba24dca56975c
original_hiro <mmahir380@gmail.com>
2021-05-26 16:37:48 -0400

    formatting

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,26 @@
-#What does a BLOCK look like?
-    # --> Each block has an index, a timestamp, list of transactions, a proof, hash of previous block(gives the immutability factor)
+import hashlib
+import json
+from time import time
 
-    # block = {
+
+class BlockChain(object):
+
+    def __init__(self):
+        # constructor creates an initially empty list to store our blockchain
+        self.chain = []
+
+        # we have another list to store current transactions
+        self.transactions = []
+
+        # the first block(genesys block) would have to be initalized with no predecessors, and proof-of-work(mining) provided
+        self.add_block(proof=100, previous_hash=1)
+
+    # consensus algorithm
+
+    # What does a BLOCK look like?
+    # --> Each block has an index, a timestamp, list of transactions, a proof,
+        # hash of previous block(gives the immutability factor)
+        # block = {
         # 'index': 1,
         # 'timestamp': 1506057125.900785, --> in unix
         # 'transactions': [
@@ -14,36 +33,17 @@
         # 'proof': 324984774000, --> I am guessing this is the so called "nonce" --> related to mining/and validity-->proof of work
         # 'previous_hash': "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
         #  what about the current hash?
-    # }
+        # }
 
+    # create a block and add to chain, returns the block(dictionary)
 
-import hashlib
-import json
-from time import time
-
-class BlockChain(object):
-
-    def __init__(self):
-        #constructor creates an initially empty list to store our blockchain
-        self.chain = []
-
-        #we have another list to store current transactions
-        self.transactions = []
-        
-        #the first block(genesys block) would have to be initalized with no predecessors, and proof-of-work(mining) provided
-        self.add_block(proof = 100, previous_hash = 1)
-    
-
-    #consensus algorithm
-
-    #create a block and add to chain, returns the block(dictionary)
-    def add_block(self, proof, previous_hash = None):
+    def add_block(self, proof, previous_hash=None):
 
         block = {
-            'index': len(self.chain)+ 1,
+            'index': len(self.chain) + 1,
             'timestamp': time(),
             'transactions': self.transactions,
-            'proof': proof , # The proof given by the Proof of Work algorithm
+            'proof': proof,  # The proof given by the Proof of Work algorithm
             'previous_hash': previous_hash or self.hash(self.latest_block)
         }
 
@@ -53,70 +53,66 @@ class BlockChain(object):
         self.chain.append(block)
         return block
 
+    # hashes the whole block
 
-    #hashes the whole block 
     @staticmethod
     def hasher(self, block):
 
-        #converts block to string, and orders the dictionary, or it'll introduce inconsistent hashes
+        # converts block to string, and orders the dictionary, or it'll introduce inconsistent hashes
         blockInString = json.dumps(block, sort_keys=True).encode()
 
-        #Creates a SHA-256 hash of a Block
+        # Creates a SHA-256 hash of a Block
         return hashlib.sha256(blockInString).hexdigest()
-
 
     def add_transaction(self, sender, receiver, mony):
 
         self.transactions.append(
             {
-            'sender': sender,
-            'recipient': receiver,
-            'amount': mony,
+                'sender': sender,
+                'recipient': receiver,
+                'amount': mony,
             }
         )
 
-        #returns the index of the block(the next one to be mined) that holds this transaction
+        # returns the index of the block(the next one to be mined) that holds this transaction
         return self.latest_block['index'] + 1
 
+    # returns the last block in the chain
 
-    #returns the last block in the chain
     @property
     def latest_block(self):
         return self.chain[-1]
 
-
-    # Using  proof-of-work algorithm(POW), new blocks are created/mined on the blockchain. The goal of PoW is to figure out 
-    # the number which would solve a problem. Computationally, this number should be difficult to find, but easy to verify 
+    # Using  proof-of-work algorithm(POW), new blocks are created/mined on the blockchain. The goal of PoW is to figure out
+    # the number which would solve a problem. Computationally, this number should be difficult to find, but easy to verify
     # by the network.
 
-    # In general, the difficulty is determined by the number of characters searched for in a string. 
+    # In general, the difficulty is determined by the number of characters searched for in a string.
 
     # --> in this case, we decided that the multiplication of hashed x and y should end with 0
     # x = 5
-    # y = 0  # We don't know what y should be 
+    # y = 0  # We don't know what y should be
 
     # so while the multiplication of the hashed values does not return a value of 0 at  index[-1] (the last character) , then increment
     # while sha256(f'{x*y}'.encode()).hexdigest()[-1] != "0":
     #     y += 1
     # print(f'The solution is y = {y}')
 
-
-    #Algorithm
+    # Algorithm
     # Find a number p' such that hash(pp') contains leading 4 0s, where p is the previous p'
     # p is the previous proof, and p' is the new proof
+
     def proof_of_work(self, last_proof):
         proof = 0
         while self.proofing(last_proof, proof) is False:
             proof += 1
         return proof
 
-
     @staticmethod
-    #returns: <bool> True if correct, False if not.
+    # returns: <bool> True if correct, False if not.
     def proofing(last_proof, proof):
 
         guess = f'{last_proof}{proof}'.encode()
         guessHashed = hashlib.sha256(guess).hexdigest()
 
         return guessHashed[:4] == "0000"
-
--- serverNode.py
+++ serverNode.py
@@ -2,82 +2,84 @@ from uuid import uuid4
 from flask import Flask, jsonify, request
 from blockchain import BlockChain
 
-# Makes it easy to map endpoints to Python functions. 
+# Makes it easy to map endpoints to Python functions.
 # This allows us talk to our blockchain over the web using HTTP requests.
 
 # Setting up Flask
-#Instantiating our Node
-app =  Flask(__name__)
+# Instantiating our Node
+app = Flask(__name__)
 
-# Our “server” will form a single node in our blockchain network. 
-#Generating a globally unique address for this particular Node
+# Our “server” will form a single node in our blockchain network.
+# Generating a globally unique address for this particular Node
 node_ID = str(uuid4()).replace('-', '')
 
-#Instantiating our blockchain
+# Instantiating our blockchain
 our_blockchain = BlockChain()
 
+# /chain to return the full Blockchain
 
-# We’ll create three methods:
-# /mine
-# to tell our server to mine a new block.
+
+@app.route('/chain', methods=['GET'])
+def chain():
+    response = {
+        'fullChain': our_blockchain.chain,
+        'length': len(our_blockchain.chain)
+    }
+    return jsonify(response), 200
+
+# /transactions/new to create a new transaction to a block
+
+
+@app.route('/transactions/new', methods=['POST'])
+def transactions():
+
+    values = request.get_json()
+
+    # checking if the required fields were in the posted data
+    required_data = ['sender', 'reciever', 'mony']
+
+    # just sees if value elements are == the required elements. If a single value is false, then returns error
+    # not sure if i understand how the for loop is structured
+    if(not all(k in values for k in required_data)):
+        return 'Missing Required Data', 400
+
+    index = our_blockchain.add_transaction(
+        values['sender'], values['reciever'], values['mony'])
+
+    response = {
+        'message': f'Transaction pending for block {index} insertion'
+    }
+
+    return jsonify(response), 201
+
+# /mine to tell our server to mine a new block.
+
+
 @app.route('/mine', methods=['GET'])
 def mine():
 
-    #Retrieving the most current block, and extracting the last proof used. We use that to get the current proof by running POW algorithm
+    # Retrieving the most current block, and extracting the last proof used. We use that to get the current proof by running PoW algorithm
     latest_block = our_blockchain.latest_block()
     last_proof = latest_block['proof']
     proof = our_blockchain.proof_of_work(last_proof)
 
     # Reward the miner (us) by adding a transaction granting us 1 coin. Sender is "0" to signify that this node has mined a new coin
-    our_blockchain.add_transaction(sender = 0, receiver = node_ID, mony = 1 )
+    our_blockchain.add_transaction(sender=0, receiver=node_ID, mony=1)
 
     # Forge the new Block by adding it to the chain
     previous_hash = our_blockchain.hasher(latest_block)
     block = our_blockchain.add_block(proof, previous_hash)
 
-    response  = {
-        "message" : "New block has been created",
+    response = {
+        "message": "New block has been created",
         'index': block['index'],
         'transactions': block['transactions'],
-        'proof': block['proof'], 
+        'proof': block['proof'],
         'previous_hash': block['previous_hash']
     }
 
     return jsonify(response), 200
 
 
-# /transactions/new
-# to create a new transaction to a block
-@app.route('/transactions/new', methods=['POST'])
-def transactions():
-
-    values = request.get_json()
-
-    #checking if the required fields were in the posted data
-    required_data = ['sender', 'reciever', 'mony']
-    
-    #just sees if value elements are == the required elements. If a single value is false, then returns error
-    if(not all(k in values for k in required_data)): ## not sure if i understand how the for loop is structured
-        return 'Missing Required Data', 400
-
-    index = our_blockchain.add_transaction(values['sender'], values['reciever'], values['mony'])
-   
-    response = {
-        'message': f'Transaction pending for block {index} insertion'
-    }
-    return jsonify(response), 201
-
-
-
-# /chain
-# to return the full Blockchain
-@app.route('/chain', methods=['GET'])
-def chain():
-    response = {
-        'fullChain' : our_blockchain.chain,
-        'length' : len(our_blockchain.chain)
-    }
-    return jsonify(response), 200
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port = 5000)
+    app.run(host='0.0.0.0', port=5000)
